d842210af029aaff8b196ba7b0bf4ebc6dbcedc7
Nathaniavm <116174777+Nathaniavm@users.noreply.github.com>
2026-03-27 14:09:03 +0100

    Update main.ino for keyboard option functionality

--- arduino-braccio-rx/main/main.ino
+++ arduino-braccio-rx/main/main.ino
@@ -1,12 +1,8 @@
 #include <Braccio.h>
 #include <Servo.h>
+#include <SoftwareSerial.h>
 
-int Gripper;
-int Gripper_ROT;
-const int msg_pin = 8;
-const int CLOSE = 73;
-const int OPEN = 10;
-
+// Braccio
 Servo base;
 Servo shoulder;
 Servo elbow;
@@ -14,55 +10,87 @@ Servo wrist_rot;
 Servo wrist_ver;
 Servo gripper;
 
+// STYRING
+int direction = 0;
+int joint = 1;
+
+int M1 = 0;
+int M2 = 45;
+int M3 = 180;
+int M4 = 0;
+int M5 = 175;
+int M6 = 73;
+const int adjustment = 2;
+const int timestep = 20;
+
+// Kommunikasjon
+String incoming = "";
+
 void setup() {
-  Serial.begin(9600);
-  pinMode(msg_pin, INPUT);
+  Serial.begin(9600);  
   Braccio.begin();
-  Gripper = OPEN;
-  Gripper_ROT = 180;
-
-  //Braccio.ServoMovement(200,         0, 43, 60, 0, 180,  10);
-  //Braccio.ServoMovement(200,         0, 43, 60, 0, 180,  180);
-  //Braccio.ServoMovement(200,         0, 43, 60, 0, 180,  180);
-  //Braccio.ServoMovement(20,         90, 43, 180, 30, 90, 10);
-  //delay(1000);
-  //Braccio.ServoMovement(20,         90, 43, 55, 30, 90, 10);
-  //delay(1000);
-  //Braccio.ServoMovement(20,         90, 43, 55, 30, 90, 73);
-  //Braccio.ServoMovement(50,         0, 43, 180, 0, 175, 73);
-
 }
 
 void loop() {
-  Braccio.ServoMovement(5, 0, 43, 180, 0, Gripper_ROT, Gripper);
-  int msg = digitalRead(msg_pin);
-  
-  /*
-  if (msg == 1) {
-     Serial.println("HIGH");
-     Gripper = CLOSE;
-   } else if (msg == 0) {
-     Serial.println("LOW");
-     Gripper = OPEN;
-  }
-  */
-  
-  if (msg == 1) {
-     Serial.println("HIGH");
-     //Gripper += 10;
-     Gripper_ROT += 10;
-   } else if (msg == 0) {
-     Serial.println("LOW");
-  }
+// ---- Les seriell data ----
+  while (Serial.available()) {
+    char c = Serial.read();
 
+    if (c == '\n') {   // ferdig melding
 
-  if (Gripper_ROT >= 180) {
-    Gripper_ROT = 0;
+      int sep = incoming.indexOf(';');
+
+      if (sep > 0) {
+        direction = incoming.substring(0, sep).toInt();
+        joint = incoming.substring(sep + 1).toInt();
+      }
+
+      incoming = "";   // tøm buffer
+    }
+    else {
+      incoming += c;   // bygg opp meldingen
+    }
   }
 
-  //if (Gripper >= 73) {
-  //  Gripper = 10;
-  //}
+  switch (joint) {
+    case (1):
+      if (direction == 2) {M1 += adjustment;}
+      else if (direction == 1) {M1 -= adjustment;}
+      M1 = constrain(M1, 0, 180);
+      break;
+
+    case (2):
+      if (direction == 2) {M2 += adjustment;}
+      else if (direction == 1) {M2 -= adjustment;}
+      M2 = constrain(M2, 15, 165);
+      break;
+
+    case (3):
+      if (direction == 2) {M3 += adjustment;}
+      else if (direction == 1) {M3 -= adjustment;}
+      M3 = constrain(M3, 0, 180);
+      break;
+
+    case (4):
+      if (direction == 2) {M4 += adjustment;}
+      else if (direction == 1) {M4 -= adjustment;}
+      M4 = constrain(M4, 0, 180);
+      break;
+
+    case (5):
+      if (direction == 2) {M5 += adjustment;}
+      else if (direction == 1) {M5 -= adjustment;}
+      M5 = constrain(M5, 0, 180);
+      break;
+
+    case (6):
+      if (direction == 2) {M6 += adjustment;}
+      else if (direction == 1) {M6 -= adjustment;}
+      M6 = constrain(M6, 10, 73);
+      break;
+  }
   
+
   delay(10);
+  Braccio.ServoMovement(timestep, M1, M2, M3, M4, M5, M6);
 }
